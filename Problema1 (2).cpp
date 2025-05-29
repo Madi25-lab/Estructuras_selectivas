f2443a7c039cb3652488c7aabcb5e03447c18960
Madi <mayradiazchambilla327@gmail.com>
2025-05-28 23:26:43 -0500

    AÑADIR el primer ejercicio de estructuras selectivas

---
+++ Problema1 (2).cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+	double C1,C2,C3,P;
+	cout<<"Ingrese la primera calificacion: " ;
+	 cin>>C1;
+	cout<<"Ingrese la segunda calificacion: ";
+	 cin>>C2;
+	cout<<"Ingrese la tercera calificacion: ";
+	 cin>>C3;
+	P=(C1+C2+C3)/3;
+	cout<<"El promedio es:"<<P<<endl;
+	if (P>=70){
+		cout << "Esta aprobado" << endl;
+	}else {
+		cout<< "Esta reprobado" << endl;
+	}
+	return 0;	
+}
