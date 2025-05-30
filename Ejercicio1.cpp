3dcd99606139fefd9399eb07a30af56ad68e1957
Madi <mayradiazchambilla327@gmail.com>
2025-05-29 23:40:38 -0500

    Mejora en el resultado salga si esta o no aprobado con su promedio junto a ello

--- Ejercicio1.cpp
+++ Ejercicio1.cpp
@@ -3,18 +3,17 @@ using namespace std;
 
 int main() {
 	double C1,C2,C3,P;
-	cout<<Ingrese la primera calificacion:  ;
+	cout<<"Ingrese la primera calificacion:  ";
 	 cin>>C1;
-	cout<<Ingrese la segunda calificacion: ;
+	cout<<"Ingrese la segunda calificacion: ";
 	 cin>>C2;
-	cout<<Ingrese la tercera calificacion: ;
+	cout<<"Ingrese la tercera calificacion: ";
 	 cin>>C3;
 	P=(C1+C2+C3)/3;
-	cout<<El promedio es:<<P<<endl;
 	if (P>=70){
-		cout << Esta aprobado << endl;
+		cout << "Esta aprobado "<< P << endl;
 	}else {
-		cout<< Esta reprobado << endl;
+		cout<< "Esta reprobado "<< P<< endl;
 	}
 	return 0;	
 }
