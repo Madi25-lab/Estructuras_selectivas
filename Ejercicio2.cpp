d7243c30fbeab472e568aebd021d8260bd768e1c
Madi <mayradiazchambilla327@gmail.com>
2025-05-29 23:21:47 -0500

    Mejora en solo permitir numeros positivos

--- Ejercicio2.cpp
+++ Ejercicio2.cpp
@@ -3,15 +3,19 @@ using namespace std;
 
 int main() {
 	double P,N,PP;
-	cout<<El precio de compra es:  ;
+	cout<<"El precio de compra es: " ;
 	 cin >> P;
-	cout<<Escoja un numero al azar:  ;
+	cout<<"Escoja un numero al azar: " ;
 	 cin >> N;
+	 if (N>=1){
 	if (N<74){
 		PP=P*0.85;
-		cout << El precio a pagar es:<<PP << endl;
+		cout << "El precio a pagar es: "<<PP << endl;
 	}else { PP=P*0.8;
-		cout << El precio a pagar es:<<PP << endl;
+		cout << "El precio a pagar es: "<<PP << endl;
 	}
+}else {
+	cout<< "El numero debe ser positivo" <<endl;
+    }
 	return 0;	
 }
