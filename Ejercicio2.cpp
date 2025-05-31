87f911b0bbedab7255188648566ab2006aa89d94
Madi <mayradiazchambilla327@gmail.com>
2025-05-31 14:58:22 -0500

    Mejora en la integracion de un string para la recepcion del nombre del comprador para brindar unmejor resultado

--- Ejercicio2.cpp
+++ Ejercicio2.cpp
@@ -1,8 +1,12 @@
 #include<iostream>
+#include <string>
 using namespace std;
 
 int main() {
+    string nombre;
 	double P,N,PP,D, M,V;
+	cout << "Nombre o DNI del comprador: ";
+    getline(cin, nombre);
 	cout<<"El precio de compra es: " ;
 	 cin >> P;
 	cout<<"Escoja un numero al azar: " ;
@@ -11,11 +15,13 @@ int main() {
 	if (N<74){
 		D=P*0.15;
 		PP=P-D;
+		cout <<nombre << endl;
 		cout << "El precio a pagar es: "<<PP << endl;
 		cout << "Su descuento fue de: "<<D << endl;
 	}else { 
 	    D=P*0.20;
 		PP=P-D;
+		cout <<nombre << endl;
 		cout << "El precio a pagar es: "<<PP << endl;
 		cout << "Su descuento fue de: "<<D << endl;
 	}
@@ -28,3 +34,5 @@ int main() {
     }
 	return 0;	
 }
+
+
