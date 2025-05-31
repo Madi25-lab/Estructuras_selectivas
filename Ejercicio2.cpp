0c92112889a4ac89caa5fa66ea9599c7af7ecc83
Madi <mayradiazchambilla327@gmail.com>
2025-05-30 23:04:47 -0500

    Mejora en poder brindar el descuento que se realiza e integrando 2 variables para saber con cuanto pagara y poder de tal manera indicar cuanto se le dara de vuelto

--- Ejercicio2.cpp
+++ Ejercicio2.cpp
@@ -2,19 +2,28 @@
 using namespace std;
 
 int main() {
-	double P,N,PP;
+	double P,N,PP,D, M,V;
 	cout<<"El precio de compra es: " ;
 	 cin >> P;
 	cout<<"Escoja un numero al azar: " ;
 	 cin >> N;
-	 if (N>=1){
+	if (N>=1){
 	if (N<74){
-		PP=P*0.85;
+		D=P*0.15;
+		PP=P-D;
 		cout << "El precio a pagar es: "<<PP << endl;
-	}else { PP=P*0.8;
+		cout << "Su descuento fue de: "<<D << endl;
+	}else { 
+	    D=P*0.20;
+		PP=P-D;
 		cout << "El precio a pagar es: "<<PP << endl;
+		cout << "Su descuento fue de: "<<D << endl;
 	}
-}else {
+	cout<<"Con cuanto pagara: " ;
+	cin>> M;
+	V=M-PP;
+	cout<<"Su vuelto es: " <<V << endl;;
+    }else {
 	cout<< "El numero debe ser positivo" <<endl;
     }
 	return 0;	
