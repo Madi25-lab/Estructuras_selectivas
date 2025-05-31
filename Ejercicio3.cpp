b7925e282d4aa70e4aac127236f36a1611eed214
Madi <mayradiazchambilla327@gmail.com>
2025-05-30 22:40:33 -0500

    Mejora en el ingreso del promedio integrando un if y else para que el intervalo de la nota se encuentre entre 0 y 20

--- Ejercicio3.cpp
+++ Ejercicio3.cpp
@@ -3,15 +3,20 @@ using namespace std;
 
 int main() {
         double P,PE,M;
-        cout<<Ingrese la pension educativa: ;
+        cout<<"Ingrese la pension educativa: ";
         cin>>PE;
-        cout<<Ingrese el promedio obtenido en el ultimo periodo: ;
+        cout<<"Ingrese el promedio obtenido en el ultimo periodo: ";
         cin>>P;
+        if (P>=0 && P<=20){
         if (P>=18){
                 M=PE*0.7;
-            cout<<El monto a pagar es de: <<M<< endl;
+            cout<<"El monto a pagar es de: "<<M<< endl;
         }else{ M=PE*1.18;
-            cout<<El monto a pagar es de: <<M<< endl;
+            cout<<"El monto a pagar es de: "<<M<< endl;
               }
+        } else {
+		cout<<"ERROR, el promedio es incorrecto "<< endl; 
+		}
+		  
         return 0;
 }
