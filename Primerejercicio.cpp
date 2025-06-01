f30b2de4c9b504481f80be61ce21db398e40d453
Madi <mayradiazchambilla327@gmail.com>
2025-06-01 14:55:29 -0500

    Mejora en la recepcion de notas dando un intervalo mediante un if, asi seguida de las demas notas si todas ellas se encuentran en el intervalo adecuado pasara a dar su promedio sino mediante un else indicara que la nota es incorrecta

--- Primerejercicio.cpp
+++ Primerejercicio.cpp
@@ -9,18 +9,30 @@ double C1,C2,C3,P;
   getline(cin, nombre);
 	cout<<"Ingrese la primera calificacion: " ;
 	 cin>>C1;
+	 if (C1>=0 && C1<=100){
 	cout<<"Ingrese la segunda calificacion: ";
 	 cin>>C2;
+	 if (C2>=0 && C2<=100){
 	cout<<"Ingrese la tercera calificacion: ";
 	 cin>>C3;
+	 if (C3>=0 && C3<=100){
 	P=(C1+C2+C3)/3;
 	if (P>=70){
 			cout <<nombre << endl;
-		cout << "Se encuentra aprobado "<< P << endl;
+		cout << "Se encuentra aprobado su promedio es: "<< P << endl;
 	}else {
 		cout <<nombre << endl;
-		cout<< "Se encuentra reprobado "<<P<< endl;
+		cout<< "Se encuentra reprobado su promedio es: "<<P<< endl;
 	}
-	return 0;	
+	}else {
+	cout<< "La nota es incorrecta debe estar entre 0 y 100 "<< endl;
+    }
+	}else {
+	cout<< "La nota es incorrecta debe estar entre 0 y 100"<< endl;
+    }
+    }else {
+	cout<< "La nota es incorrecta debe estar entre 0 y 100"<< endl;
+    }
+	return 0;
 }
 
