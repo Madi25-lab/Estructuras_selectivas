c872fc2470c1cc5a84ddcbc9ff903f4b5c6783fa
Madi <mayradiazchambilla327@gmail.com>
2025-06-02 08:17:41 -0500

    Mejora en la implementacion de un include string para la recepcion del nombre y agregando dos variables que permita saber cuanto fue el aumento o descuento en la pension educativa

--- Ejercicio3.cpp
+++ Ejercicio3.cpp
@@ -1,8 +1,12 @@
 #include<iostream>
+#include <string>
 using namespace std;
 
 int main() {
-        double P,PE,M;
+        double P,PE,M,D,A;
+        string nombre;
+        cout << "Ingresa el nombre o DNI del usuario: ";
+        getline(cin, nombre);
         cout<<"Ingrese la pension educativa: ";
         cin>>PE;
         cout<<"Ingrese el promedio obtenido en el ultimo periodo: ";
@@ -10,9 +14,13 @@ int main() {
         if (P>=0 && P<=20){
         if (P>=18){
                 M=PE*0.7;
+                D=PE-M;
             cout<<"El monto a pagar es de: "<<M<< endl;
+            cout<<"Su descuento es de: "<<D<< endl;
         }else{ M=PE*1.18;
+               A=M-PE;
             cout<<"El monto a pagar es de: "<<M<< endl;
+            cout<<"Su aumento es de: "<<A<< endl;
               }
         } else {
 		cout<<"ERROR, el promedio es incorrecto "<< endl; 
