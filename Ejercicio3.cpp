06fb10da5afa56baf3dcdc0c8420ac4062768017
maricielo <cielotapia198@gmail.com>
2025-06-07 10:03:17 -0500

    Se mejoró la interfaz del usuario para que sea mucho mas agradable visualmente y textualmente

--- Ejercicio3.cpp
+++ Ejercicio3.cpp
@@ -2,29 +2,40 @@
 #include <string>
 using namespace std;
 
-int main() {
-        double P,PE,M,D,A;
-        string nombre;
-        cout << "Ingresa el nombre o DNI del usuario: ";
-        getline(cin, nombre);
-        cout<<"Ingrese la pension educativa: ";
-        cin>>PE;
-        cout<<"Ingrese el promedio obtenido en el ultimo periodo: ";
-        cin>>P;
-        if (P>=0 && P<=20){
-        if (P>=18){
-                M=PE*0.7;
-                D=PE-M;
-            cout<<"El monto a pagar es de: "<<M<< endl;
-            cout<<"Su descuento es de: "<<D<< endl;
-        }else{ M=PE*1.18;
-               A=M-PE;
-            cout<<"El monto a pagar es de: "<<M<< endl;
-            cout<<"Su aumento es de: "<<A<< endl;
-              }
+int main(){
+double P, PE, M, D, A;
+
+cout << "=============================================" << endl;
+cout << "BIENVENIDO A MI PROGRAMA!" << endl;
+
+string nombre;
+cout << "Ingresa su NOMBRE COMPLETO porfavor: " << endl;
+getline(cin, nombre);
+
+cout<<"Ingrese el monto de la pension educativa: " << endl;
+cin>>PE;
+
+cout<<"Ingrese el promedio academico obtenido en el ultimo periodo: " << endl;
+cin>>P;
+
+  if (P >= 0 && P <= 20) {
+        cout << "Hola, " << nombre << "." << endl;
+
+        if (P >= 18) {
+            M = PE * 0.7;
+            D = PE - M;
+            cout << "El monto a pagar con descuento es: S/. " << M << endl;
+            cout << "y el descuento aplicado fue de: S/. " << D << endl;
         } else {
-		cout<<"ERROR, el promedio es incorrecto "<< endl; 
-		}
-		  
-        return 0;
+            M = PE * 1.18;
+            A = M - PE;
+            cout << "El monto a pagar con aumento es: S/ " << M << endl;
+            cout << "Aumento aplicado: S/ " << A << endl;
+        }
+    } else {
+        cout << "ERROR: El promedio ingresado es incorrecto." << endl;
+    }
+
+    cout << "\n GRACIAS POR USAR MI PROGRAMA <3!";
+    return 0;
 }
